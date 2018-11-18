4eeed7c3ff0bcc65b3a9b6add7591c50f04f1c2e
Eduardo555 <eduardospillereanzolin@gmail.com>
2018-11-18 18:49:23 -0200

    Adicionado Projeto já pré criado

---
+++ TcpCliente/ClienteEduardo/ClienteEduardo/Cliente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteEduardo
+{
+	class Cliente
+	{
+
+		public Cliente()
+		{
+			ConectarServidor();
+			EnviarMensagem();
+			FecharConexao();
+		}
+
+		public int Porta { get; set; } = 2000;
+		public string EndIpServer { get; set; } = "192.168.0.24";
+
+		// Criar o Cliente.
+		TcpClient cliente = new TcpClient();
+		private NetworkStream saida;
+		private BinaryWriter escreve;
+		private BinaryReader ler;
+		private string Mensagem = "Eureca Funciona!";
+
+		// Cria conexao com o cliente.
+		private void ConectarServidor()
+		{
+
+			cliente.Connect(EndIpServer, Porta);
+		}
+
+		// Envia Mensagem para o servidor.
+		private void EnviarMensagem()
+		{
+
+			Byte[] sendBytes = Encoding.UTF8.GetBytes("Is anybody there?");
+			saida = cliente.GetStream();
+			escreve = new BinaryWriter(saida);
+			ler = new BinaryReader(saida);
+			//MessageBox.Show(ler.ToString());
+			saida.Write(sendBytes, 0, sendBytes.Length);
+		}
+
+		// Fecha conexao com o servidor.
+		private void FecharConexao()
+		{
+			cliente.Close();
+		}
+	}
+}
---
+++ TcpServidor/ServidorEduardo/ServidorEduardo/Servidor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServidorEduardo
+{
+	class Servidor
+	{
+
+		// Criar a espera pela conexao.
+		// Espera na porta 2000.
+		TcpListener servidor = new TcpListener(2000);
+
+		public Servidor()
+		{
+			// Construtuor, inicia o sistema de comunicacao.
+			IniciarServidor();
+			EstabeleceConexao();
+			FecharConexao();
+		}
+
+
+		
+		private NetworkStream socketScreen;
+		private BinaryWriter escrita;
+		private BinaryReader leitura;
+
+		private void EstabeleceConexao()
+		{
+			// Estabelece a conexao com o cliente.
+			// Realizando o aceite de conexao.
+			Socket conexao = servidor.AcceptSocket();
+
+			// Cria Local em memoria para escrever as mensagens.
+			socketScreen = new NetworkStream(conexao);
+
+			// Esreve a mensagem.
+			escrita = new BinaryWriter(socketScreen);
+			leitura = new BinaryReader(socketScreen);
+			MessageBox.Show("Escrita: " + escrita.ToString() + " Leitura:" + leitura.ToString());
+		}
+
+		private void IniciarServidor()
+		{
+			// Inicia o servidor.
+			try
+			{
+				servidor.Start();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Erro:" + ex.Message.ToString());
+			}
+
+		}
+		private void FecharConexao()
+		{
+			// Encera a conexao.
+			try
+			{
+				servidor.Stop();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Erro:" + ex.Message.ToString());
+			}
+		}
+	}
+}
